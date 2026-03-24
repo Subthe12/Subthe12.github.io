59032b94c0241c1872b2ffc72346807a1bd39774
Subthe12 <subhayusengupta13@gmail.com>
2026-03-24 16:11:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function greetUser() {
+    // window.alert() displays an alert box in the browser
+    window.alert("Hello, World!");
+}
 
+// You can also log messages to the browser console for debugging
+console.log("JavaScript is running!"); //
